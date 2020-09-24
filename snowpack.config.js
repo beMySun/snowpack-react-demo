27de575e147a8bf32b1ec4c35d167b3ee29a6587
Yongkang Yan <yongkang.yan@shopee.com>
2020-09-24 17:08:48 +0800

    fix: snowpack version

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,20 +1,10 @@
 module.exports = {
   extends: '@snowpack/app-scripts-react',
-  scripts: {
-    'mount:public': 'mount public --to /',
-    'mount:src': 'mount src --to /_dist_',
-    'run:lint': "eslint 'src/**/*.{js,jsx,ts,tsx}'",
-    'run:lint::watch': 'watch "$1" src',
-    'build:css': 'postcss',
-    'build:js,jsx,ts,tsx': 'snowpack-plugin-import-map',
-    'bundle:*': '@snowpack/plugin-react-refresh',
-    'bundle:*': '@snowpack/plugin-webpack',
+  mount: {
+    public: '/',
+    src: '/_dist_',
   },
-  plugins: [
-    '@snowpack/plugin-react-refresh',
-    '@snowpack/plugin-webpack',
-    ['snowpack-plugin-import-map', {}],
-  ],
+  plugins: ['@snowpack/plugin-react-refresh', '@snowpack/plugin-webpack'],
   devOptions: {
     bundle: false,
   },
--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,13 @@
 import React from 'react';
 import Card from 'antd/es/card';
 import 'antd/es/card/style/css';
-import { isString } from 'lodash-es';
 import logo from './logo.png';
-import styles from './App.module.css';
-
-class Store {
-  public title: string = '';
-  public desc: string = '';
-  constructor({ title, desc }: { title?: string; desc?: string }) {
-    this.title = isString(title) ? title : 'Hello World!';
-    this.desc = isString(desc) ? desc : '';
-  }
-}
-
-const store = new Store({
-  desc: 'snowpack 初尝试，学习和实践；',
-});
 
 export default () => {
   return (
     <Card hoverable style={{ textAlign: 'center', margin: 40 }}>
-      <div className={styles.title}>{store.title}</div>
-      <div className={styles.desc}>{store.desc}</div>
-      <img className={styles.img} src={logo} />
+      <div> Hello Snowpack !!!~~</div>
+      <img src={logo} />
     </Card>
   );
 };
